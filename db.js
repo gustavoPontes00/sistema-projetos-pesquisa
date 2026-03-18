116f9490757dbdb61d5ae9d9d9341eba4c95a150
Gustavopontes00 <gustavo.p0ntes017@gmail.com>
2026-03-17 23:43:59 -0300

    Sprint 2 - Banco de dados criado

--- db.js
+++ db.js
@@ -6,10 +6,7 @@ const connection = mysql.createConnection({
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
